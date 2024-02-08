a55da069f1fc49126f51d7d115d87a0708c5dfb9
YAOSTEPHANE <kouassistephane489@gmail.com>
2024-02-08 11:15:45 +0000

    frontend

--- batirem-frontend/src/actions/product.action.js
+++ batirem-frontend/src/actions/product.action.js
@@ -16,6 +16,20 @@ export const getProductsBySlug = (slug) => {
                 //payload: { error: res.data.error }
             //});
         }
+    }
+}
+
+export const getProductPage = (payload) => {
+    return async dispatch => {
+
+        const {cid, type} = payload;
+        const res = await axios.get(`/page/${cid}/${type}`);
+        console.log(res);
+        if(res.status ===200){
+            
+        } else{
+           
+        }
        
     }
 }
--- batirem-frontend/src/components/MenuHeader/index.js
+++ batirem-frontend/src/components/MenuHeader/index.js
@@ -24,11 +24,12 @@ const MenuHeader = (props) => {
       myCategories.push(
         <li key={category.name}>
           {
-            category.parentId ? <a href={category.slug}>
-            {category.name}
+            category.parentId ? <a
+              href={`/${category.slug} ? cid=${category._id}&type=${category.type}`}>
+              {category.name}
             </a> : <span>{category.name}</span>
           }
-          
+
           {category.children.length > 0 ? (<ul>{renderCategories(category.children)}</ul>) : null}
         </li>
       );
